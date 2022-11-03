5ac2b60161b09a21b967506875fdd73e6a507c13
Daniel Son <dson.email@gmail.com>
2022-11-03 15:34:32 +1100

    Update variable names and components can return messages

--- index.js
+++ index.js
@@ -1,50 +1,41 @@
 const body = document.querySelector("body");
 
-const dialogs = document.querySelectorAll("dialogArea");
+const dialogs = document.querySelectorAll("dialogComponent");
 
-const dialogMessage = document.querySelector(".dialogArea div");
+const dialogMessage = document.querySelector(".dialogComponent div");
 const dialogYesButton = document.querySelector(
-  ".dialogArea button:first-child"
+  ".dialogComponent button:first-child"
 );
 const dialogCancelButton = document.querySelector(
-  ".dialogArea button:last-child"
+  ".dialogComponent button:last-child"
 );
 
 const dialogConsumers = document.querySelectorAll(".dialogConsumer");
 console.log(dialogConsumers.length);
 
 dialogConsumers.forEach((consumer) => {
-  const dialogMessage = consumer.getAttribute("data-message");
-  const triggerButton = consumer.querySelector("button");
+  const messageForComponent = consumer.getAttribute("data-message");
+  const componentOutputMessage = consumer.querySelector("article");
+  const componentTriggerButton = consumer.querySelector("button");
 
-  const dialogArea = consumer.querySelector(".dialogArea");
-  const dialogMessageArea = document.querySelector(".dialogArea section div");
+  const component = consumer.querySelector(".dialogComponent");
+  const componentMessage = component.querySelector("section div");
 
-  const dialogYesButton = consumer.querySelector(
-    ".dialogArea button:first-child"
-  );
-  const dialogNoButton = consumer.querySelector(
-    ".dialogArea button:last-child"
-  );
+  const dialogYesButton = component.querySelector("button:first-of-type");
+  const dialogNoButton = component.querySelector("button:last-of-type");
 
-  triggerButton.addEventListener("click", () => {
-    //showDialog(dialogMessage);
-    dialogArea.classList.add("show");
-    dialogMessageArea.textContent = dialogMessage;
-    console.log(dialogArea);
+  componentTriggerButton.addEventListener("click", () => {
+    component.classList.add("show");
+    componentMessage.textContent = messageForComponent;
+  });
+
+  dialogYesButton.addEventListener("click", () => {
+    component.classList.remove("show");
+    componentOutputMessage.textContent = "You just clicked 'Yes'";
+  });
+
+  dialogNoButton.addEventListener("click", () => {
+    component.classList.remove("show");
+    componentOutputMessage.textContent = "You just clicked 'No'";
   });
 });
-
-/* function showDialog(message) {
-    console.log(message);
-    console.log(dialog);
-    dialog.classList.add("show");
-
-    dialogMessage.textContent = message;
-}
-
-function closeDialog(isYesClicked) {
-    dialog.classList.remove("show");
-
-    return isYesClicked ? 'You just clicked "Yes"' : 'You just clicked "Cancel"';
-} */
